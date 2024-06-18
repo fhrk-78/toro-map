81d19916c4b0f1054838d3e670891b29131ede2d
PizzaHarumaki <78fhruby78@gmail.com>
2024-06-18 20:14:19 +0900

    change not hash and fix transparent bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory /*createWebHistory*/ } from 'vue-router'
+import { createRouter, createWebHistory /*createWebHistory*/ } from 'vue-router'
 
 import About from '@/views/AboutView.vue'
 import Main from '@/views/MainView.vue'
@@ -7,8 +7,7 @@ import Theme from '@/views/ThemeView.vue'
 import Settings from '@/views/SettingsView.vue'
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    //NOT HASH: history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/about',
