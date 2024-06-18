62acf4b6d99087f28613aae32d7f8907aec51a76
PizzaHarumaki <78fhruby78@gmail.com>
2024-06-18 21:39:42 +0900

    (再ビルド)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory /*createWebHistory*/ } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory /*createWebHistory*/ } from 'vue-router'
 
 import About from '@/views/AboutView.vue'
 import Main from '@/views/MainView.vue'
@@ -7,7 +7,8 @@ import Theme from '@/views/ThemeView.vue'
 import Settings from '@/views/SettingsView.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
+    //history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/about',
